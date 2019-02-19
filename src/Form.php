1640c363d6b30df05697786ec6b2338e416785e2
xaboy <xaboy2005@qq.com>
2019-02-19 21:17:22 +0800

    #2

--- src/Form.php
+++ src/Form.php
@@ -10,14 +10,44 @@ namespace LaravelFormBuilder;
 
 class Form extends \FormBuilder\Form
 {
+
     /**
      * Form constructor.
+     *
      * @param string $action
-     * @param array $components
+     * @param array  $components
+     * @param bool   $token
      */
-    public function __construct($action = '', array $components = [])
+    public function __construct($action = '', array $components = [], $token = true)
     {
         parent::__construct($action, $components);
-        $this->append(\FormBuilder\Form::hidden('_token',csrf_token()));
+        if ($token)
+            $this->append(\FormBuilder\Form::hidden('_token', csrf_token()));
+
+        $this->init();
+    }
+
+    protected function init()
+    {
+        $this->script = [
+            'jq' => asset('vendor/form-create/jquery.js'),
+            'vue' => asset('vendor/form-create/vue/vue.min.js'),
+            'iview-css' => asset('vendor/form-create/iview/styles/iview.css'),
+            'iview' => asset('vendor/form-create/iview/iview.min.js'),
+            'form-create' => asset('vendor/form-create/form-create.min.js'),
+            'city-data' => asset('vendor/form-create/province_city.js'),
+            'city-area-data' => asset('vendor/form-create/province_city_area.js')
+        ];
+    }
+
+    /**
+     * @param string $action
+     * @param array  $components
+     * @param bool   $token
+     * @return \FormBuilder\Form|mixed
+     */
+    public static function create($action, array $components = [], $token = true)
+    {
+        return self::create($action, $components, $token);
     }
 }
